0aecabe6027df744a0d4ec17494fdafd3cfd20ef
BeqaBexo <beqacaava1997@gmail.com>
2023-04-21 23:17:38 +0400

    Add project files.

---
+++ OlAcademy_Example3/Program.cs
@@ -0,0 +1,26 @@
+﻿
+/*დავალება 3: ორი სხვადასხვა მეთოდის შქმნა */
+class Program
+{
+    static int MethodS(int length, int width)
+    {
+        return length * width;
+    }
+
+    static int MethodV(int length, int width, int height)
+    {
+        return length * width * height;
+    }
+
+    static void Main(string[] args)
+    {
+
+        int s = MethodS(3, 4);
+        int v = MethodV(3, 4, 5);
+
+        Console.Write("-----------------------------------------------------\n");
+        Console.Write("-----------------------------------------------------\n");
+        Console.WriteLine($"Method 1: length * width * height =  {s}");
+        Console.WriteLine($"Method 2: length * width = {v}");
+    }
+}
